2fbeb9805e4836717f617f3f4ae3028df8ee9cd8
이순현 <sunwl123@naver.com>
2022-05-22 14:27:24 +0900

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
  
 module.exports = {
  
+    name: 'react-project',
+    
+    mode: 'Production',
+ 
     resolve: {
         extensions: ['.js', '.jsx']
     },
@@ -57,4 +61,21 @@ module.exports = {
         filename: 'bundle.js',
         publicPath: '/dist'
     },
+ 
+    devServer: {
+        proxy: {
+            '/api': {
+            target: 'https://hnm-express-project.herokuapp.com/',
+            changeOrigin: true,
+            }
+        },
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        compress: true,
+        port: 3000,
+        hot: true,
+        historyApiFallback: true 
+    },
+ 
 }
