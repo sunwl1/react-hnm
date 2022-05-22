9991e79dffc9a77408c398418669852a7b6dc712
이순현 <sunwl123@naver.com>
2022-05-22 14:09:26 +0900

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -69,13 +69,6 @@ module.exports = {
             changeOrigin: true,
             }
         },
-        static: {
-            directory: path.join(__dirname, 'public')
-        },
-        compress: true,
-        port: 3000,
-        hot: true,
-        historyApiFallback: true 
     },
  
 }
