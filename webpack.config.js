3091c693917c9501e74db8626a87a3baf1a40e93
이순현 <sunwl123@naver.com>
2022-05-22 14:32:32 +0900

    ok

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const webpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
  
 module.exports = {
+ 
+    name: 'react-project',
+ 
     resolve: {
         extensions: ['.js', '.jsx']
     },
@@ -10,26 +13,67 @@ module.exports = {
     entry: {
         app: ['./src/index.jsx']
     },
-
+ 
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader', 
+            options: {
+                presets: [
+                    ['@babel/preset-env', {  
+                        targets: {
+                            browsers: [  
+                                'last 2 chrome versions',
+                                '> 5% in KR'
+                            ]
+                        },
+                        debug: true
+                    }],  
+                    '@babel/preset-react'  
+                ],
+                plugins: [
+                    'react-refresh/babel'
+                ]
+            }
+        },
+        {
+            test: /\.css$/,
+            use: [{
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    //
+                }
+            },
+            'css-loader'
+            ]
+        }]
+    },
+ 
+    plugins: [
+        new webpackPlugin(),
+        new MiniCssExtractPlugin({ filename: 'style.css' }) 
+    ],
+ 
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: '/dist'
     },
-    
-
-    module: {
-            loaders: [
-                {
-                    test: /\.js$/,
-                    loader: 'babel-loader',
-                    exclude: /node_modules/,
-                    query: {
-                        cacheDirectory: true,
-                        presets: ['es2015', 'react']
-                    }
-                }
-            ]
-    }
+ 
+    devServer: {
+        proxy: {
+            '/api': {
+            target: 'https://hnm-express-project.herokuapp.com/',
+            changeOrigin: true,
+            }
+        },
+        static: {
+            directory: path.join(__dirname, 'public')
+        },
+        compress: true,
+        port: 3000,
+        hot: true,
+        historyApiFallback: true 
+    },
  
 }
