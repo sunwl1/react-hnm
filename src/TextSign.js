41bc9e8b335f016d94f01edc191ff22bc393b9c1
이순현 <sunwl123@naver.com>
2022-05-22 18:57:37 +0900

    ok

--- src/TextSign.js
+++ src/TextSign.js
@@ -5,6 +5,6 @@ export default class TextSign {
 		this.elem.classList.add('text-sign');
 		this.elem.style.transform = `translate(${Math.random() * window.innerWidth * 0.8}px, ${Math.random() * window.innerHeight * 0.8}px)`;
 		document.body.appendChild(this.elem);
-		document.body.appendChild(<box/>);
+		document.body.appendChild(box);
 	}
 }
